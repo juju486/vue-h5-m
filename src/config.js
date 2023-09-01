43528eb0fc78660090d6b5ffe132aebca99f89b2
juju <2947332905@qq.com>
2023-09-01 08:34:08 +0800

    配置修改

--- src/config.js
+++ src/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
   // 开发环境配置
   development: {
-    baseUrl: 'https://app2.zhangzhengyun.com',
+    baseUrl: '',
   },
   // 生产环境配置
   production: {
-    baseUrl: 'https://app2.zhangzhengyun.com',
+    baseUrl: '',
   }
 };
 export default CONFIG[process.env.NODE_ENV];
