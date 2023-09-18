2628874d89d0d41d7c037a3468f349a8686db974
juju <2947332905@qq.com>
2023-09-18 13:14:36 +0800

    压缩改为vite社区插件

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
-import compress from "./src/plugins/vite-plugin-compress";
+import zipPack from "vite-plugin-zip-pack";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    compress(__dirname),
+    zipPack({ outDir: 'dist' }),
     vue(),
     vueJsx(),
     Components({
